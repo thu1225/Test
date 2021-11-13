42f318468e17862c792e66231643ef5092c87e28
VuVT <truongvuuit@gmail.com>
2021-11-13 10:52:44 +0700

    Review

--- buoi1.rb
+++ buoi1.rb
@@ -3,13 +3,13 @@ while numbers.length <100 do
     begin
         puts "Nhap so nguyen: "
         
-        a = Integer(gets)
+        a = Integer(gets) # biến a cần đặt có ý nghĩa hơn
         rescue
         puts "Khong hop le. Thu lai voi gia tri khac....."
         retry
         end
 
-        if a == -1 then break
+        if a == -1 then break # không cần dùng then nhé, nên xuống dòng cho dễ nhìn hơn
         elsif a >= 0 then numbers.push(a)
         else puts " Nhap so tu nhien"
         end
@@ -17,8 +17,11 @@ end
 
 puts "Gia tri mang la : #{numbers}"
 min = numbers[0]
-numbers.each do |b| min = b if b < min
+numbers.each do |b| min = b if b < min # nên xuống dòng
     puts b
 end
 
 puts "Phan tu mang gia tri nho nhat la: #{min}"
+    
+# Chương trình e viết tốt, biến numbers biết đặt là số nhiều
+# Một vài biến e nên đặt cho có ý nghĩa hơn nhé, cũng nên chú ý xuống dòng để dễ đọc
