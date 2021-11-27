e6b011e60828816fc82bef2ca4812f8a4daa963f
VuVT <truongvuuit@gmail.com>
2021-11-27 09:31:48 +0700

    Review ex6

--- buoi6.rb
+++ buoi6.rb
@@ -14,3 +14,6 @@ for i in 0..arr_numbers.length
 end
 
 puts "Hai phan tu co tong bang #{target_sum} la: " + two_numbers.to_s
+# Code e viết ngắn gọn, giải thuật ok
+# Khi e sử dụng include? để check có phần tử nào khác trong mảng có giá trị cần tìm kg thì cũng như là e dùng dòng for thứ 2 rồi nhé e, 
+# nhưng cũng ok, nhưng vd sau này mảng có hàng triệu phần tử mà e sử dụng include? như vậy sẽ chạy hơi lâu nhé
