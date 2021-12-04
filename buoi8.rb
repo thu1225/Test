4a4a3996e04517f687ffa05663baf1756934f053
VuVT <truongvuuit@gmail.com>
2021-12-04 11:11:59 +0700

    Review ex8

--- buoi8.rb
+++ buoi8.rb
@@ -6,3 +6,8 @@ while sub == b
     puts true
     break
 end 
+# A rất bất ngờ với cách giải quyết bài toán này của e, quá nhanh gọn.
+# Mình không biết là khi sử dụng a & b để tìm phần tử chung của cả 2 mảng thì bên dưới ruby đang duyệt ntn, dùng mấy vòng for, và sau này 
+# khó để optimize hơn nữa.
+# và e có thể viết 1 dòng cũng được: puts b == a & b
+# Tên biến e nên đặt có ý nghĩa hơn nhé
