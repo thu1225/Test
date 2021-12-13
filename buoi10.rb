fe9cbe7d6f50509628d50d6839834af127216764
thu1225 <92871749+thu1225@users.noreply.github.com>
2021-12-13 15:09:38 +0700

    [Ex10] Update bai tap so 10

--- buoi10.rb
+++ buoi10.rb
@@ -2,29 +2,25 @@ def three_numbers(arrays, target_sum)
     three_elements =[]
    
 
-    for i in (0..arrays.length)
+    for value1 in (0...arrays.length)
         hash = {}
-        first_element = arrays[i]
+        first_element = arrays[value1]
         total_two_elements = target_sum - first_element
-        array1 = arrays -arrays[i]
-
-        for n in (0..array1.length)
-            mid_element = array1[n]
-            
-            array1.each do |last_element|
-            if hash[total_two_elements - mid_element]
-                return [last_element]
-            else
-                hash [last_element] = true
-            end
+        array_sub = arrays - [arrays[value1]]
+        
+        for value2 in ((value1 + 1)...array_sub.length)
+            mid_element = array_sub[value2]
+            last_element = total_two_elements - mid_element
+                
+                if hash[last_element]
+                    puts "#{first_element} #{mid_element} #{last_element}"
+                else
+                    hash [mid_element] = true
+                end
         end
     end
-    three_elements = first_element + mid_element + last_element
-    puts "#{three_elements}"
 end
 
-numbers = [12, 3, -1, -2, -6, 5, -8, 6]
+numbers = [12, 3, 1, 2, -6, 5, -8, 6]
 target_sum = 0
 three_numbers(numbers,target_sum)
-  
- # Anh ơi em phát triển bài toán theo hướng này, nhưng có vẻ nó đã bị sai. Anh có thể cho em xin góp ý để em làm lại ạ.
